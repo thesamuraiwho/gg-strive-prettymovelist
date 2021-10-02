e37155f48e13e1a318fb1588e6b20781ef44cd48
Brandon Le <thesamuraiwho@gmail.com>
2021-10-02 06:28:55 -0700

    Adding filtering of the majority of the page in scrape.

--- get-moves.py
+++ get-moves.py
@@ -1,3 +1,4 @@
+# Data collected from https://strategywiki.org/wiki/Guilty_Gear_Strive/Moves under  Creative Commons Attribution-ShareAlike
 import requests
 from bs4 import BeautifulSoup
 import json
@@ -15,21 +16,49 @@ soup = BeautifulSoup(req.text, "html.parser")
 mainContent = soup.find("div", attrs={"class": "mw-parser-output"})
 print(mainContent.prettify())
 print(type(mainContent))
-# for c in mainContent.children:
-#     print(c)
 
 print(mainContent.contents)
 print(len(mainContent.contents))
 
-for i in range(len(mainContent.contents)):
-    print(f"{i}\t{mainContent.contents[i]}")
-
+# Removing the end of mainContent
 mainContent.contents = mainContent.contents[:223]
-print(type(mainContent.contents[0]))
+# for i in range(len(mainContent.contents)):
+#     if(mainContent.contents[i] == '\n'):
+#         print(f"{i}\t{mainContent.contents[i]}")
+
+# Removing the new lines
+mainContent.contents = [i for i in mainContent.contents if i != "\n"]
+# print(mainContent.contents)
+# for i in range(len(mainContent.contents)):
+#     print(f"{i}\t{mainContent.contents[i]}")
+    
+print(f"len of mainContent: {len(mainContent.contents)}")
+
+# Removing <p><br></p>
+print(mainContent.contents[115])
+print(mainContent.contents[115].contents)
+print(type(mainContent.contents[115]))
+# Create a matching tag but something is different
+soup = BeautifulSoup()
+para = soup.new_tag("p")
+br = soup.new_tag("br")
+para.append(br)
+para.append("\n")
+print(para)
+
+for i in range(len(mainContent.contents)):
+    if(mainContent.contents[i] == para):
+        print(f"{i}\t{mainContent.contents[i]}")
+
+mainContent.contents = [i for i in mainContent.contents if i != para]
+
+print(f"len of mainContent: {len(mainContent.contents)}")
+
+# print(type(mainContent.contents[0]))
 
 # mainContent.contents = [i for i in mainContent.contents if re.match("\S", i)]#list(filter(lambda x: re.match("\S", x), mainContent.contents))
 
-print(f"len of mainContent: {len(mainContent.contents)}")
+# print(f"len of mainContent: {len(mainContent.contents)}")
 
 # for i in range(len(mainContent.contents)):
 #     print(f"{i}\t{mainContent.contents[i]}")
