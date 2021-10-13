8dfae36c561f736e0fe9bd85c21a578b6646116c
Brandon Le <thesamuraiwho@gmail.com>
2021-10-12 17:16:31 -0700

    Refactoring code by removing dead code.

--- get-moves.py
+++ get-moves.py
@@ -59,127 +59,38 @@ def findMoves(char, charMoves):
 site = "https://strategywiki.org/wiki/Guilty_Gear_Strive/Moves"
 req = requests.get(site)
 
-# print(resp.text)
 soup = BeautifulSoup(req.text, "html.parser")
-# headlines = soup.find_all('h2', {'class': 'mw-headline'})
-# print(headlines)
-# print(soup.title)
-# mainContent = soup.find("div", attrs={"id": "mw-content-text"})
 mainContent = soup.find("div", attrs={"class": "mw-parser-output"})
-# print(mainContent.prettify())
-# print(type(mainContent))
-
-# print(mainContent.contents)
-# print(len(mainContent.contents))
 
 # Removing the beginning and end of mainContent and getting the section with moves
 mainContent.contents = mainContent.contents[4:223]
-# for i in range(len(mainContent.contents)):
-#     if(mainContent.contents[i] == '\n'):
-#         print(f"{i}\t{mainContent.contents[i]}")
-
-# for i in range(5):
-#     print(f"{i}\t{mainContent.contents[i]}")
 
 # Removing the new lines
 mainContent.contents = [i for i in mainContent.contents if i != "\n"]
-# print(mainContent.contents)
-# for i in range(len(mainContent.contents)):
-#     print(f"{i}\t{mainContent.contents[i]}")
-    
-# print(f"len of mainContent: {len(mainContent.contents)}")
 
 # Removing <p><br></p>
-# print(mainContent.contents[115])
-# print(mainContent.contents[115].contents)
-# print(type(mainContent.contents[115]))
-# Create a matching tag but something is different
-# soup = BeautifulSoup()
 para = soup.new_tag("p")
 br = soup.new_tag("br")
 para.append(br)
 para.append("\n")
-# print(para)
-
-# for i in range(len(mainContent.contents)):
-#     if(mainContent.contents[i] == para):
-#         print(f"{i}\t{mainContent.contents[i]}")
 
 mainContent.contents = [i for i in mainContent.contents if i != para]
 
-# print(f"len of mainContent: {len(mainContent.contents)}")
-
-# for i in range(len(mainContent.contents)):
-#     print(f"{i}\t{mainContent.contents[i]}\n\n")
-
 # Removing this element: <div style="clear: both;"></div>
-
-# soup = BeautifulSoup()
 clearDiv = soup.new_tag("div")
 clearDiv['style'] = "clear: both;"
-# print(clearDiv)
-
-# for i in range(len(mainContent.contents)):
-#     if(mainContent.contents[i] == clearDiv):
-#         print(f"{i}\t{mainContent.contents[i]}")
 
 mainContent.contents = [i for i in mainContent.contents if i != clearDiv]
-# print(f"len of mainContent: {len(mainContent.contents)}")
-
-# for i in range(len(mainContent.contents)):
-#     print(f"{i}\t{mainContent.contents[i]}\n\n")
 
 # Removing the div with the character's thumbnail <div class="floatright">
-
 floatDiv = soup.new_tag("div")
 floatDiv["class"] = "floatright"
-# print(floatDiv)
-
-# print(mainContent.contents[66])
-# print(f"\n\nmainContent.contents[66].name: {mainContent.contents[66].name}\n\nmainContent.contents[66].attrs: {mainContent.contents[66].attrs}\n")
-# print(mainContent.contents[66].__dict__)
-# print(mainContent.contents[66].__dict__.keys())
-
-# for i in range(len(mainContent.contents)):
-#     if(mainContent.contents[i].name == "div" and mainContent.contents[i].attrs == {'class': ['floatright']}):
-#       print(f"{i}\t{mainContent.contents[i]}\n\n")
-
 mainContent.contents = [i for i in mainContent.contents if i.names != "div" and i.attrs != {'class': ['floatright']}]
-# print(f"len of mainContent: {len(mainContent.contents)}\n\n")
-
-# for i in range(len(mainContent.contents)):
-#     print(f"{i}\t{mainContent.contents[i]}\n\n")
 
 baseChars = mainContent.contents[:45]
 dlcChars = mainContent.contents[46:]
 
-# for i in range(len(dlcChars)):
-    # print(f"{i}\t{dlcChars[i]}\n\n")
-
 chars = {}
-
-# for i in range(len(baseChars)):
-#     print(f"\n{i}\n{baseChars[i]}")
-
-print("\n\n")
-
-# ### Issue with millia (27) and ramlethal (36) command normals names.
-# ### SOLUTION: Wasn't an issue at all. Just a print formatting thing.
-# count = 24
-
-# print(baseChars[count])
-# print(baseChars[count + 1])
-# print(baseChars[count + 2])
-
-# charName = baseChars[count].contents[0]["id"]
-# charMoves = {"command normals": OrderedDict(), "special attacks": OrderedDict(), 
-#     "overdrives": OrderedDict()}
-
-# findMoves(baseChars[count + 1], charMoves)
-# findMoves(baseChars[count + 2], charMoves)
-# print(charName)
-# pp.pprint(charMoves)
-
 
 ### WORKING LOOP
 
@@ -210,25 +121,13 @@ while(count < len(dlcChars)):
 
 commandImgs = set()
 
-
-
 for char in chars.values():
     for v in char.values(): # Move types (dicts)
-        # print(v)
         for v1 in v.values(): # Moves (lists)
-            # print(v1)
             for i in range(len(v1)): # Commands (strs)
-                # print(v2)
                 if re.search(".png$", v1[i]):
                     commandImgs.add(v1[i])
                     v1[i] = f"assets/command-imgs/{v1[i].split('/')[-1]}"
-                    print(v1[i])
-
-pp.pprint(chars['Faust'])
-
-# print(commandImgs)
-# print(len(commandImgs))
-# print(type(commandImgs))
 
 # Download all the command icons
 path = "assets/command-imgs"
@@ -250,7 +149,3 @@ else:
 # Write out final json
 with open("gg-strive-moves.json", "w") as json_file:
     json.dump(chars, json_file, indent=2)
-
-
-# print(len(chars))
-# pp.pprint(chars['Faust'])
